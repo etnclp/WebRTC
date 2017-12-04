bcb0ccd53e005c119e3dfae88db4b3adde99487e
Erdi Tunçalp <etnclp@Erdis-MacBook-Pro.local>
2017-12-04 14:40:27 +0300

    Initial commit

---
+++ WebRTC/WebRTC.h
@@ -0,0 +1,48 @@
+//
+//  WebRTC.h
+//  WebRTC
+//
+//  Created by Erdi Tunçalp on 4.12.2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WebRTC.
+FOUNDATION_EXPORT double WebRTCVersionNumber;
+
+//! Project version string for WebRTC.
+FOUNDATION_EXPORT const unsigned char WebRTCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WebRTC/PublicHeader.h>
+
+
+#import "RTCPeerConnectionFactory.h"
+#import "RTCEAGLVideoView.h"
+#import "RTCAVFoundationVideoSource.h"
+#import "RTCAudioSource.h"
+#import "RTCAudioTrack.h"
+#import "RTCDataChannel.h"
+#import "RTCEAGLVideoView.h"
+#import "RTCI420Frame.h"
+#import "RTCICECandidate.h"
+#import "RTCICEServer.h"
+#import "RTCMediaConstraints.h"
+#import "RTCMediaSource.h"
+#import "RTCMediaStream.h"
+#import "RTCMediaStreamTrack.h"
+//#import "RTCNSGLVideoView.h"
+#import "RTCOpenGLVideoRenderer.h"
+#import "RTCPair.h"
+#import "RTCPeerConnection.h"
+#import "RTCPeerConnectionDelegate.h"
+#import "RTCPeerConnectionFactory.h"
+#import "RTCPeerConnectionInterface.h"
+#import "RTCSessionDescription.h"
+#import "RTCSessionDescriptionDelegate.h"
+#import "RTCStatsDelegate.h"
+#import "RTCStatsReport.h"
+#import "RTCTypes.h"
+#import "RTCVideoCapturer.h"
+#import "RTCVideoRenderer.h"
+#import "RTCVideoSource.h"
+#import "RTCVideoTrack.h"
---
+++ WebRTCTests/WebRTCTests.m
@@ -0,0 +1,38 @@
+//
+//  WebRTCTests.m
+//  WebRTCTests
+//
+//  Created by Erdi Tunçalp on 4.12.2017.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WebRTCTests : XCTestCase
+
+@end
+
+@implementation WebRTCTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
